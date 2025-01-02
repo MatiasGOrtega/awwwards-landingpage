5ec62ac40d00375f94172adc7106a6cfd873842e
MatiasOrtega <mat-1998-ort@hotmail.com>
2025-01-02 19:29:37 -0300

    Add About section and AnimatedTitle component; update Hero section styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import About from "./sections/About"
 import Hero from "./sections/Hero"
 
 function App() {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
       <Hero />
+      <About />
     </main>
   )
 }
