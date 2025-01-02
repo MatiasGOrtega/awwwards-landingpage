5dbb9301857388436fcdfa20ccd815bc70578dc5
MatiasOrtega <mat-1998-ort@hotmail.com>
2025-01-02 18:05:37 -0300

    Refactor App component to use Hero section; add LoaderSpinner, Button, and VideoPreview components; update ESLint config and package dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import Hero from "./sections/Hero"
 
 function App() {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
-      App
+      <Hero />
     </main>
   )
 }
